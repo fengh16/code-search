eb4e9a5b8a58fb286aa29cc82595331f071a5881
Sun Ziping <sunziping2016@gmail.com>
2019-07-23 05:59:09 +0000

    rearrange transformers

--- README.md
+++ README.md
@@ -11,7 +11,10 @@ This project is intended to search code with natural language.
   - `extracted`: contains csvs for extracted dirty data
   - `cleaned`: contains csvs that is cleaned
   - `transformed`: contains folder whose inside files are suitable for training
+  - `models`: contains folder that contains saved models
 - `extractors`: scripts used to extract data from raw code
+- `cleaners`: scripts used to clean data from extracted data
+- `transformers`: script used to translate cleaned data to model input
 
 ## How to Run This Project
 
@@ -24,7 +27,19 @@ Please put source code under `data/raw`. The root of the source code should be e
 Currently only Python and JavaScript are supported.
 
 ```bash
-./extractor --extract {python,javascript} --dataset <dataset>
+./extractor.py --extractor {python,javascript} --dataset <dataset>
 ```
 
 Replace `<dataset>` with the root directory name of you data.
+
+### 3 Clean Extracted Data
+
+```bash
+./cleaner.py --dataset <dataset>
+```
+
+### 4 Translate Cleaned Data to Model Input
+
+```bash
+./transformer.py --transformer {codenn} --dataset <dataset>
+```
--- transformers/codenn.py
+++ transformers/codenn.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 import numpy as np
 import tables
 import os
 import sys
 import pickle
+import argparse
 from sklearn import model_selection
 
-def build_vocab(series, num=10000):
+def build_vocab(series, max_vocab_size):
     vocab_num = {}
     series = list(map(lambda x: x.split('|'), list(series)))
     for i in series:
@@ -15,8 +19,10 @@ def build_vocab(series, num=10000):
     vocab_rev = list(vocab_num.items())
     vocab_rev.sort(key=lambda x: x[1], reverse=True)
     vocab_rev = list(map(lambda x: x[0], vocab_rev))
-    vocab_rev = vocab_rev[:num - 2]
-    return {v: k + 2 for k, v in enumerate(vocab_rev)}, [''] + vocab_rev
+    vocab_rev = vocab_rev[:max_vocab_size - 2]
+    # 0: padding; 1: unknown; 2...: vocabulary
+    return ({v: k + 2 for k, v in enumerate(vocab_rev)},
+            ['<PAD>', '<UNK>'] + vocab_rev)
 
 def save_to_h5(series, vocab, file):
     with tables.open_file(file, mode='w') as h5f:
@@ -37,7 +43,7 @@ def save_to_h5(series, vocab, file):
             array.append(list(map(lambda x: vocab.get(x, 1), item)))
             pos += length
 
-def process(input, output):
+def process(input, output, max_vocab_size):
     if not os.path.exists(output):
         os.makedirs(output)
     origin_data = pd.read_csv(input)
@@ -55,21 +61,28 @@ def process(input, output):
     tasks = [('train', train), ('valid', validate), ('test', test), ('use', origin_data)]
     vocabs = [];
     for name, select in series:
-        vocab, vocab_rev = build_vocab(select(origin_data))
+        vocab, vocab_rev = build_vocab(select(origin_data), max_vocab_size)
+        print('%s vocabulary size: %d' % (name, len(vocab_rev)))
         with open(os.path.join(output, 'vocab.%s.pkl' % name), 'wb') as f:
             pickle.dump(vocab, f)
         with open(os.path.join(output, 'vocabrev.%s.pkl' % name), 'wb') as f:
             pickle.dump(vocab_rev, f)
         vocabs.append(vocab)
     for task_name, task in tasks:
+        print('%s set size: %d' % (task_name, task.shape[0]))
         for (series_name, select), vocab in zip(series, vocabs):
+            if task_name == 'use' and series_name == 'desc':
+                continue
             save_to_h5(select(task), vocab, os.path.join(output,
                 '%s.%s.h5' % (task_name, series_name)))
     origin_data[['file', 'start', 'end']].to_csv(os.path.join(output, 'use.codemap.csv'),
                                                  index=False)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("transform.py <input_file> <output_path>")
-    else:
-        process(sys.argv[1], sys.argv[2])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='input csv that contains cleaned data')
+    parser.add_argument('output', help='output path that dataset should be generated into')
+    parser.add_argument('--max_vocab_size', help='max vocabulary size',
+                        type=int, default=10000)
+    args = parser.parse_args()
+    process(args.input, args.output, args.max_vocab_size)
